0cf76c8d099c60e3bb30f5df60409319dac4a0b4
YongJieYongJie <KhooYongJie@gmx.com>
2018-02-25 01:14:32 +0800

    Add basic controller-to-template

--- executor/main/views.py
+++ executor/main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    object_passed_to_template = {'key': 'value'}
+    template = loader.get_template('main/index.html')
+    context = {
+        'object_passed_to_template': object_passed_to_template,
+    }
+    return HttpResponse(template.render(context, request))
 
