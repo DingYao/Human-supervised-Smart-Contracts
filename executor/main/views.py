4115aa479061219c433e55b5f5c9d84e3b9ed558
IcyDragon <jerrold_is_not@hotmail.com>
2018-02-25 15:56:57 +0800

    user dashboard notifs and quick actions

--- executor/main/views.py
+++ executor/main/views.py
@@ -15,7 +15,12 @@ def index(request):
 def usrDashboard(request):
     template = loader.get_template('main/usr_dashboard.html')
     context = {
-        'user': { 'user_type': 1 },
+        'user': { 'user_type': 1, 
+                  'notifications': ['Contract u203917731232aadsasdae3ada has paid out $1000 to Justin as scheduled on 21/11/2017', 
+                                    'Protector psd2132183572132135 has begun reviewing Contract u203917731232aadsasdae3ada', 
+                                    ],
+                  'actions' : ['Revoke', 'Revoke']
+                },
     }
     return HttpResponse(template.render(context, request))
 
