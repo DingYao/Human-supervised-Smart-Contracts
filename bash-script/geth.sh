3ca48cd27a0cee796c572d6a51736daff1625600
Ubuntu <gethadmin@geth18czww4a-tx0.uygbfuh3udjedeuy1xvteyyy0g.ix.internal.cloudapp.net>
2018-02-25 09:58:11 +0000

    Yo!

--- bash-script/geth.sh
+++ bash-script/geth.sh
@@ -2,8 +2,8 @@
 #!/usr/bin/expect
 exec solcjs --abi test.sol
 exec solcjs --bin test.sol
-set abi [exec more test_sol_getSchwifty.abi]
-set bin [exec more test_sol_getSchwifty.bin]
+set abi [exec more test_sol_startup.abi]
+set bin [exec more test_sol_startup.bin]
 spawn /usr/bin/geth attach
 expect ">"
 send "eth.accounts\r"
@@ -11,15 +11,15 @@ expect ">"
 send "personal.unlockAccount(eth.coinbase)\r"
 send "gLhT2018gLhT\r"
 expect ">"
-send "var getSchwifty = eth.contract($abi)\r"
+send "var getstartup = eth.contract($abi)\r"
 expect ">"
 send "var bytecode = '0x$bin'\r"
 expect ">"
 send "var deploy = {from:eth.coinbase, data:bytecode, gas: 2000000}\r"
 expect ">"
-send "var getSchwiftyPartialInstance = getSchwifty.new(\"DISQUALIFIED!\", deploy)\r"
+send "var getstartupPartialInstance = getstartup.new(deploy)\r"
 expect ">"
-exec sleep 45
-send "getSchwiftyPartialInstance.address\r"
+interact
+send "getstartupPartialInstance.address\r"
 send "exit\r"
 expect eof
--- bash-script/test.sol
+++ bash-script/test.sol
@@ -1,20 +1,61 @@
-pragma solidity 0.4.20;
+contract startup{  
+  mapping (address => uint) balances;
 
-contract getSchwifty {
-  string internal constant seeWhatYouGot = "I want to see what you got!";
-  string internal contestResult;
-  
-  // Contract constructor that takes a string param
-  function getSchwifty(string _contestResult) public {
-    contestResult = _contestResult;
+  address public settlor;
+  address public beneficiary;
+  address public court;
+  address public protector;
+  address public startup = msg.sender;
+  bool settlorApprove;
+  bool courtApprove;
+
+  function setup(address settlor, address beneficiary, address protector, address court){
+    if(msg.sender == startup){
+        settlor = settlor;
+        beneficiary = beneficiary;
+	protector = protector;
+	court = court;
+    }
   }
-  
-  function showMeWhatYouGot() external pure returns (string) {
-    return seeWhatYouGot;  
+
+  function approve(){
+    if(msg.sender == settlor) settlorApprove = true;
+    else if(msg.sender == court) courtApprove = true;
+    if(settlorApprove || courtApprove) fee();
   }
-  
-  function enterContest(string _contestEntry) external view returns (string) {
-    // completely disregard the _contestEntry ;)
-    return contestResult;
+
+  function abort(){
+      if(msg.sender == settlor) settlorApprove = false;
+      else if (msg.sender == court) courtApprove = false;
+      if(!settlorApprove && !courtApprove) fee();
+      if(!settlorApprove && !courtApprove) refund();
   }
-} 
+
+  function payOut(){
+    fee();
+    if(beneficiary.send(this.balance)) balances[settlor] = 0;
+  }
+
+  function deposit(){
+      if(msg.sender == settlor) {
+	balances[settlor] += msg.value;
+	fee();
+      }
+      else throw;
+  }
+
+  function killContract() internal {
+      selfdestruct(court);
+      //kills contract and returns funds to court
+  }
+
+  function refund(){
+    if(courtApprove == false && settlorApprove == false) selfdestruct(settlor);
+    //send money back to settlor if both parties agree will is void
+  }
+
+  function fee(){
+      startup.send(this.balance / 100); //1% fee
+  }
+
+}
