c38332b02c3b5c38174c18b0184f61a164c24465
IcyDragon <jerrold_is_not@hotmail.com>
2018-02-25 16:49:23 +0800

    standardise all dashbaords and add contractdetails

--- executor/main/urls.py
+++ executor/main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('user/dashboard', views.usrDashboard, name='usrDashboard'),
     path('protector/dashboard', views.ptrDashboard, name='ptrDashboard'),
     path('court/dashboard', views.crtDashboard, name='crtDashboard'),
+    path('contract/jorda', views.contractDetail, name='contractDetail'),
 ]
 
--- executor/main/views.py
+++ executor/main/views.py
@@ -16,8 +16,8 @@ def usrDashboard(request):
     template = loader.get_template('main/usr_dashboard.html')
     context = {
         'user': { 'user_type': 1, 
-                  'notifications': ['Contract u203917731232aadsasdae3ada has paid out $1000 to Justin as scheduled on 21/11/2017', 
-                                    'Protector psd2132183572132135 has begun reviewing Contract u203917731232aadsasdae3ada', 
+                  'notifications': ['Contract u2039 has paid out $1000 to Justin as scheduled on 21/11/2017', 
+                                    'Protector psd21 has begun reviewing Contract u2039', 
                                     ],
                   'actions' : ['Revoke', 'Revoke']
                 },
@@ -52,6 +52,7 @@ def ptrDashboard(request):
     context = {
         'user': { 'user_type': 2,
                   'contracts': contracts,
+                  'actions' : ['Approve', 'Review'] 
                 },
     }
     return HttpResponse(template.render(context, request))
@@ -59,13 +60,29 @@ def ptrDashboard(request):
 def crtDashboard(request):
     template = loader.get_template('main/crt_dashboard.html')
     context = {
-        'user': { 'user_type': 3 },
+        'user': { 'user_type': 3, 
+                  'notifications': ['Contract 123ea has requested SIAC review citing: \"Help I forgot my private key\"', 
+                                    'Contract glh18 has requested SIAC review citing: \"Please help me revoke a frustrated contract\"',
+                                    'Contract h4xor has requested SIAC review citing: \"Monies paid out twice. Code is buggy\"', 
+                                    'Contract 3152a has requested SIAC to be revoked citing: \"Was defrauded to enter smart contract!\"',
+                                    'Contract we412 has requested SIAC review citing: \"Was defrauded to enter smart contract!\"', 
+                                    ],
+                  'actions' : ['Ignore', 'Review', 'Ignore', 'Revoke', 'Ignore']
+                },
     }
     return HttpResponse(template.render(context, request))
 
 def contractDetail(request):
     template = loader.get_template('main/contract_detail.html')
     context = {
+        'summary' : {'Created' : '25 Feb 2018',
+                     'Gifts Made': '2',
+                     'Initial Amount Transferred': '20000',
+                     'Remaining Balance': '10000',
+                     'Owner Supervision': 'Revoke Only',
+                     'Protector Supervision': 'Approval Only',
+                     'SIAC Supervision': 'Revise Only',
+                    }
 
     }
     return HttpResponse(template.render(context, request))
